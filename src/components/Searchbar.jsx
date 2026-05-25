512c5093a26e262bd66646ae220e2944b77b4235
aiminnano <gabriellesmateo@gmail.com>
2026-05-26 00:07:57 +0800

    updated design

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,24 @@
 import bgdesktop from '../../images/pattern-bg-desktop.png'
 import bgmobile from '../../images/pattern-bg-mobile.png'
+import arrow from '../../images/icon-arrow.svg'
+import Result from './Result'
 
 function Searchbar(){
     return (
         <>
-            <div className="grid place-items-center">
+            <div className="grid place-items-center font-rubik">
                 <div className="w-full max-w-[1440px]">
-                    <img src={bgdesktop} className='hidden min-[376px]:block absolute z-1' alt="bg-desktop" />
-                    <img src={bgmobile} className='min-[376px]:hidden absolute z-1' alt="bg-desktop" />
-                    <div className='relative flex flex-col items-center z-2 p-5 gap-5'>
-                        <h1 className='font-medium text-white text-[27px]'>IP Address Tracker</h1>
-                        <input type="text" className='pt-3 pr-5 pb-3 pl-5 bg-white rounded-xl' placeholder='Search for any IP address or domain' />
+                    <img src={bgdesktop} className='hidden sm:block absolute z-1' alt="bg-desktop" />
+                    <img src={bgmobile} className='sm:hidden absolute z-1 w-full' alt="bg-desktop" />
+                    <div className='relative z-2 flex flex-col gap-6 p-5.5 items-center sm:gap-12'>   
+                        <div className='flex flex-col items-center gap-5 w-full sm:gap-3.5'>
+                            <h1 className='font-semibold text-white text-[26px] sm:text-[32px]'>IP Address Tracker</h1>
+                            <div className='h-[58px] flex w-full mt-2 sm:max-w-[500px]'>
+                                <input type="text" className='w-full pr-5 pl-5 bg-white rounded-l-[.90rem] h-full text-lg'  placeholder='Search for any IP address or domain' />
+                                <button type='button' className=' pr-5  pl-5 bg-black rounded-r-[.95rem] h-full max-w-[56px] w-full flex items-center justify-center'><img src={arrow}  alt="arrow-icon" /></button>
+                            </div>
+                        </div>
+                        <Result />
                     </div>
                 </div>
             </div>
