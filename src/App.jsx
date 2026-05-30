e51594f6a9a861ac7c28fc57a1136fec01cfe8f1
aiminnano <gabriellesmateo@gmail.com>
2026-05-30 16:12:27 +0800

    updated the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import Searchbar from "./components/Searchbar"
 
+import { useState, useEffect } from 'react';
+
+
 function App() {
+
+  const [ip, setIp] = useState('');
+  const [ipData, setIpData] = useState(null);
+
+    const handleSearch = () => {
+        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_obzWDQlJg2JSO9Zbg9ph9Yz7CAL7Q&ipAddress=${ip}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setIpData(data);
+        })
+        
+    };
 
   return (
     <>
-      <Searchbar />
+      <Searchbar ip={ip} setIp={setIp} onSearch={handleSearch} ipData={ipData}/>
     </>
   )
 }
